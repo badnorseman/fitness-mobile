2da73e1f91eea18d05aa1628ca0ea2ccc3eacd56
Dan Jensen <dj@fitbird.com>
2016-02-23 16:29:44 -0500

    update habit_actions, habit_api and habit reducer

--- app/actions/habit_actions.js
+++ app/actions/habit_actions.js
@@ -1,45 +1,11 @@
+'use strict';
 import * as actionTypes from '../constants/action_types';
 import { appError, appReceive } from './app_actions';
 import { request } from './network_actions';
-import { loadAll, loadStarted, check, start } from '../api/habits';
-
+import { loadAll, loadStarted, check, start } from '../api/habit_api';
 import processHabitsStarted from '../utils/processHabitsStarted';
 
-export function habitsLoadAll() {
-  return (dispatch) => {
-    dispatch({ type: actionTypes.HABITS_LOAD_ALL });
-
-    return request(loadAll())(dispatch)
-      .then(response => response.json())
-      .then(json => {
-        dispatch(appReceive(
-          json,
-          actionTypes.HABITS_LOAD_ALL_SUCCESS,
-          actionTypes.HABITS_LOAD_ALL_FAIL
-        ));
-      })
-      .catch(error => appError(error));
-  };
-}
-
-export function habitsLoadStarted() {
-  return (dispatch) => {
-    dispatch({ type: actionTypes.HABITS_LOAD_STARTED });
-
-    return request(loadStarted())(dispatch)
-      .then(response => response.json())
-      .then(json => {
-        dispatch(appReceive(
-          processHabitsStarted(json),
-          actionTypes.HABITS_LOAD_STARTED_SUCCESS,
-          actionTypes.HABITS_LOAD_STARTED_FAIL
-        ));
-      })
-      .catch(error => appError(error));
-  };
-}
-
-export function habitsCheck(id, occurence) {
+const habitsCheck = (id, occurence) => {
   return (dispatch) => {
     const props = {
       id: id,
@@ -61,9 +27,43 @@ export function habitsCheck(id, occurence) {
       )))
       .catch(error => appError(error));
   };
-}
+};
 
-export function habitsStart(id) {
+const habitsLoadAll = () => {
+  return (dispatch) => {
+    dispatch({ type: actionTypes.HABITS_LOAD_ALL });
+
+    return request(loadAll())(dispatch)
+      .then(response => response.json())
+      .then(json => {
+        dispatch(appReceive(
+          json,
+          actionTypes.HABITS_LOAD_ALL_SUCCESS,
+          actionTypes.HABITS_LOAD_ALL_FAIL
+        ));
+      })
+      .catch(error => appError(error));
+  };
+};
+
+const habitsLoadStarted = () => {
+  return (dispatch) => {
+    dispatch({ type: actionTypes.HABITS_LOAD_STARTED });
+
+    return request(loadStarted())(dispatch)
+      .then(response => response.json())
+      .then(json => {
+        dispatch(appReceive(
+          processHabitsStarted(json),
+          actionTypes.HABITS_LOAD_STARTED_SUCCESS,
+          actionTypes.HABITS_LOAD_STARTED_FAIL
+        ));
+      })
+      .catch(error => appError(error));
+  };
+};
+
+const habitsStart = (id) => {
   return (dispatch) => {
     dispatch({
       type: actionTypes.HABITS_START,
@@ -81,4 +81,6 @@ export function habitsStart(id) {
       })
       .catch(error => appError(error));
   };
-}
+};
+
+export { habitsCheck, habitsLoadAll, habitsLoadStarted, habitsStart };
