a04ec74d78ded90f5a509e73de7d22f26eb97225
Dan Jensen <dj@fitbird.com>
2016-02-25 16:14:04 -0500

    update App, Main and HabitList

--- app/containers/Main.js
+++ app/containers/Main.js
@@ -1,4 +1,3 @@
-'use strict';
 import React, { Component, Navigator, View, StyleSheet } from 'react-native';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
@@ -8,7 +7,7 @@ import EditWorkout from '../components/EditWorkout';
 import Exercise from '../components/Exercise';
 import ExerciseHistory from '../components/ExerciseHistory';
 import FitbirdStatusBar from '../components/FitbirdStatusBar';
-import Habits from '../components/Habits';
+import HabitList from '../components/HabitList';
 import Login from '../components/Login';
 import NavigationBarRouteMapper from '../components/NavBar';
 import ShowHabit from '../components/ShowHabit';
@@ -21,10 +20,18 @@ import * as dashboardActions from '../actions/dashboard_actions';
 import * as habitActions from '../actions/habit_actions';
 import * as planActions from '../actions/plan_actions';
 
-class Main extends Component {
-  constructor(props) {
-    super(props);
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignSelf: 'stretch'
   }
+});
+
+class Main extends Component {
+  static propTypes = {
+    dispatch: React.PropTypes.func,
+    state: React.PropTypes.object
+  };
 
   componentWillMount() {
     this.props.dispatch(authActions.authByCookie());
@@ -37,11 +44,13 @@ class Main extends Component {
       nextProps.dispatch(planActions.planLoad());
     }
 
-    if (nextProps.state.auth.ok && !Object.keys(nextProps.state.habit.all).length && !nextProps.state.habit.all.loading && !nextProps.state.habit.all.loaded) {
+    if (nextProps.state.auth.ok && !Object.keys(nextProps.state.habit.all).length &&
+      !nextProps.state.habit.all.loading && !nextProps.state.habit.all.loaded) {
       nextProps.dispatch(habitActions.getHabits());
     }
 
-    if (nextProps.state.auth.ok && !Object.keys(nextProps.state.habit.started).length && !nextProps.state.habit.started.loading && !nextProps.state.habit.started.loaded) {
+    if (nextProps.state.auth.ok && !Object.keys(nextProps.state.habit.started).length &&
+      !nextProps.state.habit.started.loading && !nextProps.state.habit.started.loaded) {
       nextProps.dispatch(habitActions.getStartedHabits());
     }
 
@@ -57,7 +66,8 @@ class Main extends Component {
     }
 
     if (nextProps.state.auth.ok === false && nextProps.state.auth.ok !== this.props.state.auth.ok) {
-      nextProps.dispatch(appActions.alert('Authentication Failed', 'Please enter a valid e-mail and password'));
+      nextProps.dispatch(
+        appActions.alert('Authentication failed', 'Please enter valid email and password'));
       nextProps.dispatch(authActions.clear());
       return nextProps;
     }
@@ -70,7 +80,7 @@ class Main extends Component {
       sharedProps,
       route.props,
       {
-        navigator: navigator,
+        navigator,
         state: this.props.state,
         dispatch: this.props.dispatch
       }
@@ -78,26 +88,42 @@ class Main extends Component {
 
     switch (route.id) {
       case 'dashboard':
-        return <Dashboard {...sharedProps} {...bindActionCreators({ ...dashboardActions, ...habitActions }, this.props.dispatch)}/>;
+        return (<Dashboard {...sharedProps}
+          {...bindActionCreators({ ...dashboardActions, ...habitActions }, this.props.dispatch)}
+        />);
       case 'feedback':
-        return <EditFeedback {...sharedProps} {...bindActionCreators({ ...planActions }, this.props.dispatch)} />;
+        return (<EditFeedback {...sharedProps}
+          {...bindActionCreators({ ...planActions }, this.props.dispatch)}
+        />);
       case 'edit':
-        return <EditWorkout {...sharedProps} {...bindActionCreators({ ...planActions }, this.props.dispatch)} />;
+        return (<EditWorkout {...sharedProps}
+          {...bindActionCreators({ ...planActions }, this.props.dispatch)}
+        />);
       case 'exercise':
-        return <Exercise {...sharedProps} {...bindActionCreators({ ...planActions, ...countersActions }, this.props.dispatch)} />;
+        return (<Exercise {...sharedProps}
+          {...bindActionCreators({ ...planActions, ...countersActions }, this.props.dispatch)}
+        />);
       case 'history':
         return <ExerciseHistory {...sharedProps} />;
       case 'habits':
-        return <Habits {...sharedProps} {...bindActionCreators({ ...habitActions }, this.props.dispatch)} />;
+        return (<HabitList {...sharedProps}
+          {...bindActionCreators({ ...habitActions }, this.props.dispatch)}
+        />);
       case 'habit':
-        return <ShowHabit {...sharedProps} {...bindActionCreators({ ...habitActions }, this.props.dispatch)} />;
+        return (<ShowHabit {...sharedProps}
+          {...bindActionCreators({ ...habitActions }, this.props.dispatch)}
+        />);
       case 'video':
         return <Video {...sharedProps} />;
       case 'workout':
-        return <Workout {...sharedProps} {...bindActionCreators({ ...planActions }, this.props.dispatch)} />;
+        return (<Workout {...sharedProps}
+          {...bindActionCreators({ ...planActions }, this.props.dispatch)}
+        />);
       case 'login':
       default:
-        return <Login {...sharedProps} {...bindActionCreators({ ...authActions }, this.props.dispatch)}/>;
+        return (<Login {...sharedProps}
+          {...bindActionCreators({ ...authActions }, this.props.dispatch)}
+        />);
     }
   }
 
@@ -112,7 +138,7 @@ class Main extends Component {
             backgroundColor: 'rgba(46, 49, 58, 1)'
           }}
           initialRoute={{ id: 'login', title: 'Fitbird' }}
-          renderScene={this.renderScene.bind(this)}
+          renderScene={this.renderScene}
           navigationBar={
             <Navigator.NavigationBar
               routeMapper={NavigationBarRouteMapper}
@@ -121,23 +147,11 @@ class Main extends Component {
               }}
             />
           }
-          logout={() => this.props.dispatch(authActions.logout())}
+          logout={this.props.dispatch(authActions.logout())}
         />
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignSelf: 'stretch'
-  }
-});
-
-Main.propTypes = {
-  dispatch: React.PropTypes.func,
-  state: React.PropTypes.object
-};
-
-export default connect(state => ({ state: state }))(Main);
+export default connect(state => ({ state }))(Main);
