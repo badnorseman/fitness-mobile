25c55c9e133852195af38e4b0efe27d1a05bf7cd
Dan Jensen <dj@fitbird.com>
2016-02-25 17:17:24 -0500

    update habit api

--- app/api/habit.js
+++ app/api/habit.js
@@ -1,32 +1,30 @@
 import HEADERS from '../constants/headers';
 import { SERVER } from '../constants/server';
 
-const check = (id, occurence) => {
-  fetch(`${SERVER}/habit/${id}/${occurence.week}/${occurence.day}/check`, {
+export function check(id, occurence) {
+  return fetch(`${SERVER}/habit/${id}/${occurence.week}/${occurence.day}/check`, {
     method: 'POST',
     headers: HEADERS
   });
-};
+}
 
-const fetchAll = () => {
-  fetch(`${SERVER}/habit/all`, {
+export function fetchAll() {
+  return fetch(`${SERVER}/habit/all`, {
     method: 'GET',
     headers: HEADERS
   });
-};
+}
 
-const fetchStarted = () => {
-  fetch(`${SERVER}/habit/started`, {
+export function fetchStarted() {
+  return fetch(`${SERVER}/habit/started`, {
     method: 'GET',
     headers: HEADERS
   });
-};
+}
 
-const start = (id) => {
-  fetch(`${SERVER}/habit/${id}/start`, {
+export function start(id) {
+  return fetch(`${SERVER}/habit/${id}/start`, {
     method: 'POST',
     headers: HEADERS
   });
-};
-
-export { check, fetchAll, fetchStarted, start };
+}
