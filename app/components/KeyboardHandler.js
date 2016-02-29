2734d24eba0307465f1dbbe2b1adc9724a25877f
Dan Jensen <dj@fitbird.com>
2016-02-29 12:11:05 -0500

    fix linting for KeyboardHandler

--- app/components/KeyboardHandler.js
+++ app/components/KeyboardHandler.js
@@ -11,54 +11,47 @@
  *        ...
  *      </View>
  *    </KeyboardHandler>
- * 
+ *
  *  offset is optional and defaults to 34
  *  Any other specified props will be passed on to ScrollView
  */
-'use strict';
+import React, { DeviceEventEmitter, ScrollView, View } from 'react-native';
 
-import React, {
-  ScrollView,
-  View,
-  DeviceEventEmitter,
-} from 'react-native';
-
-const myprops={ 
-  offset:34,
-};
+const myprops = { offset: 34 };
 
 export default React.createClass({
-  propTypes:{
-    offset: React.PropTypes.number,
+  propTypes: {
+    offset: React.PropTypes.number
   },
-  
+
   getDefaultProps() {
     return myprops;
   },
-  
+
   getInitialState() {
     this.scrollviewProps = {
-      automaticallyAdjustContentInsets:true,
-      scrollEventThrottle:200
+      automaticallyAdjustContentInsets: true,
+      scrollEventThrottle: 200
     };
-    
+
     // pass on any props we don't own to ScrollView
-    Object.keys(this.props).filter((n)=>{return n!='children'})
-    .forEach((e)=>{if(!myprops[e])this.scrollviewProps[e]=this.props[e]});
+    Object.keys(this.props).filter((n) => { return n !='children' }).forEach((e) => {
+      if (!myprops[e]) this.scrollviewProps[e] = this.props[e];
+    });
 
     return {
-      keyboardSpace:0,
+      keyboardSpace: 0
     };
   },
 
   componentDidMount() {
-    DeviceEventEmitter.addListener('keyboardDidShow',(frames) => {
+    DeviceEventEmitter.addListener('keyboardDidShow', (frames) => {
       if (!frames.endCoordinates) return;
-      this.setState({keyboardSpace: frames.endCoordinates.height});
+      this.setState({ keyboardSpace: frames.endCoordinates.height });
     });
-    
-    DeviceEventEmitter.addListener('keyboardWillHide', (frames) => {
-      this.setState({keyboardSpace:0});
+
+    DeviceEventEmitter.addListener('keyboardWillHide', () => {
+      this.setState({ keyboardSpace: 0 });
     });
   },
 
@@ -67,23 +60,24 @@ export default React.createClass({
     DeviceEventEmitter.removeAllListeners('keyboardWillHide');
   },
 
-  render(){
-    return (
-      <ScrollView ref='scrollView' {...this.scrollviewProps}>
-        {this.props.children}
-        <View style={{height:this.state.keyboardSpace}}></View>
-      </ScrollView>
-    );
-  },
-
-  inputFocused(_this,refName){
-    setTimeout(()=>{
-      let scrollResponder=this.refs.scrollView.getScrollResponder();
+  inputFocused(_this, refName) {
+    setTimeout(() => {
+      const scrollResponder = this.refs.scrollView.getScrollResponder();
       scrollResponder.scrollResponderScrollNativeHandleToKeyboard(
         React.findNodeHandle(_this.refs[refName]),
-        this.props.offset, //additionalOffset
+        this.props.offset,
         true
       );
     }, 50);
+  },
+
+
+  render() {
+    return (
+      <ScrollView ref="scrollView" {...this.scrollviewProps}>
+        {this.props.children}
+        <View style={{ height: this.state.keyboardSpace }}></View>
+      </ScrollView>
+    );
   }
 });
